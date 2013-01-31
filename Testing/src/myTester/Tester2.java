3edc73ac4efee89412bd44f8cdaa482ffa2bcf39
irvin <ihomem@gmail.com>
2013-01-31 23:17:45 +0100

    First Commit

---
+++ Testing/src/myTester/Tester2.java
@@ -0,0 +1,17 @@
+package myTester;
+
+public class Tester2 {
+
+	public Tester2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing again");
+	}
+
+}
---
+++ Testing/src/myTester/myTester.java
@@ -0,0 +1,17 @@
+package myTester;
+
+public class myTester {
+
+	public myTester() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing");
+	}
+
+}
